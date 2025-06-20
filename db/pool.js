755dbf2c036e04e9b3ba239495c631b90557ccbd
SaraToth <saratoth101@gmail.com>
2025-06-20 20:24:53 +0900

    Feat: Create pool.js connection to database

---
+++ db/pool.js
@@ -0,0 +1,8 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const localDatabaseURL = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
+module.exports = new Pool({
+    connectionString: process.env.DATABASE_URL || localDatabaseURL,
+});
