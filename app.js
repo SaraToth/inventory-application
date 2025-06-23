06fb3923db009d5869a42d0b3abdd28ad40b0e07
SaraToth <saratoth101@gmail.com>
2025-06-24 08:59:17 +0900

    Feat: Iniate views, configure app and router

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+const indexRouter = require("./routes/indexRouter");
+const path = require("node:path");
 
-app.get("/", (req, res) => {
-    res.send("Project started!");
-});
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use("/", indexRouter);
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
